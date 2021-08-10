a490815f1e051adba85eb833306569da69eaa83f
lPdFUzZT <thatelitemaili33t@gmail.com>
2021-08-10 16:43:03 +0300

    initial commit

--- server.js
+++ server.js
@@ -9,6 +9,21 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // parse requests of content-type - application/json
 app.use(bodyParser.json())
 
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
+
 // define a simple route
 app.get('/', (req, res) => {
     res.json({"message": "Welcome to MrFlava's application. Take notes quickly. Organize and keep track of all your notes."});
