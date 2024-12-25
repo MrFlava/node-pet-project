0733023a762342f77825cdf98af204594f6b5573
MrFlava <thatelitemaili33t@gmail.com>
2024-12-25 12:06:25 +0200

    Node and file system (directories)

--- files.js
+++ files.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 
+// reading files
 fs.readFile('./docs/blog.txt', (err, data) => {
     if (err){
         console.log(err);
@@ -7,8 +8,25 @@ fs.readFile('./docs/blog.txt', (err, data) => {
     console.log(data.toString());
 });
 
-// console.log('last line')
-
+// writing files
 fs.writeFile('./docs/blog1.txt', 'hello world', () => {
     console.log('file was writtnen')
 });
+
+// directories
+if (!fs.existsSync('./assets')) {
+    fs.mkdir('./assets', (err) => {
+        if (err){
+            console.log(err);
+        }
+
+        console.log('folder created')
+    })
+} else {
+    fs.rmdir('./assets', (err) => {
+        if (err){
+            console.log(err);
+        }
+        console.log('folder removed')
+    })
+}
