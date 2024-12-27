91037243c12929a1a1e7d32c58d5a6b702d41077
MrFlava <thatelitemaili33t@gmail.com>
2024-12-27 14:08:19 +0200

    Requests & responses, return html pages

--- serverExample/server.js
+++ serverExample/server.js
@@ -1,13 +1,21 @@
 const http = require('http')
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
 
     // set header content type and write
     res.setHeader('Content-Type', 'text/html');
-    res.write('<p>helllooooooo!</p>');
-    res.write('<p>again!</p>');
-    res.end();
+
+    // send an html file
+    fs.readFile('./views/index.html', (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end('error');
+        } else {
+            res.end(data);
+        }
+    })
 });
 
 server.listen(3000, 'localhost', (err) => {
