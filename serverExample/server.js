ba719cfce1c8f3223c6197d2e5f87d0eb37faea3
MrFlava <thatelitemaili33t@gmail.com>
2024-12-27 14:01:11 +0200

    Requests & responses, response obj

--- serverExample/server.js
+++ serverExample/server.js
@@ -2,6 +2,12 @@ const http = require('http')
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
+
+    // set header content type and write
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<p>helllooooooo!</p>');
+    res.write('<p>again!</p>');
+    res.end();
 });
 
 server.listen(3000, 'localhost', (err) => {
