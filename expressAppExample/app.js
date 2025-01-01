f8765c697b1590587218c1c6eb7a32c9fb140c8a
MrFlava <thatelitemaili33t@gmail.com>
2025-01-01 19:34:24 +0200

    MongoDB work, outputting documents in views

--- expressAppExample/app.js
+++ expressAppExample/app.js
@@ -38,50 +38,22 @@ app.use((req, res, next) => {
     next();
 })
 
-app.get('/add-blog', (req, res) => {
-    const blog = new Blog({
-        title: 'New Blog',
-        snippet: 'about some stuff',
-        body: 'Lorem ipsum dolor dollar',
-    });
-
-    blog.save().then((result) => {
-        res.send({result})
-    }).catch((err) => {
-        console.log(err);
-    });
-})
-
-app.get('/all-blogs', (req, res) => {
-    Blog.find({}).then((result) => {
-        res.send(result);
-    }).catch((err) => {
-        console.log(err);
-    })
-})
-
-app.get('/blog/:id', (req,res) => {
-    Blog.findById(req.params.id).then((result) => {
-        res.send(result)
-    }).catch((err) => {
-        console.log(err);
-    }).catch((err) => {
-        console.log(err);
-    })
-})
-
 app.get('/', (req, res) => {
-    const blogs = [
-        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    ];
-    res.render('index', {title: 'Express server example', blogs});
+   res.redirect('/blogs')
 });
 
 app.get('/about', (req, res) => {
     res.render('about', {title: 'About'});
 });
+
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({createdAt: -1})
+        .then((blogs) => {
+        res.render('index', {title: 'All blogs', blogs});
+    }).catch((err) => {
+        console.log(err);
+    })
+})
 
 app.get('/blogs/create', (req, res) => {
     res.render('create', {title: 'Create blog'})
